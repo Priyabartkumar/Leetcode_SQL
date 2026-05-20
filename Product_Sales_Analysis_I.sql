78dfdf15ef4c760da75510a5bc5fd9a1434a4be9
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-05-20 23:36:36 +0530

    Update Product_Sales_Analysis_I.sql

--- Product_Sales_Analysis_I.sql
+++ Product_Sales_Analysis_I.sql
@@ -11,3 +11,14 @@
 product_id is a foreign key (reference column) to Product table.
 Each row of this table shows a sale on the product product_id in a certain year.
 Note that the price is per unit.
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
