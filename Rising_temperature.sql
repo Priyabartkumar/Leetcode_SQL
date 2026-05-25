fad5ff1b658cc9cfc47be9176f093ec6318199c0
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-05-25 23:26:16 +0530

    Update Rising_temperature.sql

--- Rising_temperature.sql
+++ Rising_temperature.sql
@@ -44,5 +44,7 @@ Output:
 Explanation: 
 In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
 In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+Queries:
  
 
